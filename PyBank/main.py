031d89a83b4f6a58e4aa1c8f9bee70327718bcac
SidDas <siddas@ucdavis.edu>
2022-01-28 23:13:13 -0800

    Solved # of months and net total. Then I had to make multiple adjustments to the variables before the loop because the loop needed to change to begin at the second row instead of the first row. The change was necessary to account for the average monthly change.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,10 @@
 # Read CSV file
 
-# The os module will allow us to create file paths across operating systems
+# Import necessary modules
 import os
-# Module for reading CSV files
 import csv
+
+from sympy import li
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
@@ -12,6 +13,43 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
  
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first
     csvheader = next(csvreader)
-    print(f"Header: {csvheader}")
+    
+# Create variables to store relevant data
+
+    # We must read the first row before the loop because the calculation for monthly change must begin from the second row
+    firstline = next(csvreader)
+    firstmonthamount = int(firstline[1])
+
+    # First month is equal to 1 instead of 0 because the loop starts at the second row
+    numberofmonths = 1
+    
+    # Calculation for net amount must include first month's data
+    netprofitorloss = firstmonthamount
+
+    # Create lists to track average changes in "Profit/Losses" over the entire period
+    listformonthlychange = []
+
+    for row in csvreader:
+
+        # Calculate total number of months included in the data set
+        numberofmonths += 1
+
+        # Calculate total amount of "Profit/Losses" over the entire period
+        netprofitorloss += int(row[1])
+
+        # Calculate average changes in "Profit/Losses" over the entire period
+        monthlychange = int(row[1]) - firstmonthamount
+        listformonthlychange.append(monthlychange)
+        firstmonthamount = int(row[1])
+
+
+    # Calculate monthly change per month
+    averagemonthlychange = round(sum(listformonthlychange) / len(listformonthlychange), 2)
+    
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months: {numberofmonths}")
+    print(f"Total: ${netprofitorloss}")
+    print(f"Average Change: ${averagemonthlychange}")
